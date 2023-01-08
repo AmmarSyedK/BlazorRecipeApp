ec91c11a1dc5fbea6684282bcaf7d8e3fa9f0ac0
AmmarSyedK <ammarsyedk@gmail.com>
2023-01-08 19:08:54 +0100

    First commit

---
+++ Entity/RecipeItem.cs
@@ -0,0 +1,10 @@
+namespace RecipeApp.Entity
+{
+    public class Recipe
+    {
+        public string Id { get => Id; set => Id = value; }
+        public string Name { get => Name; set => Name = value; }
+        public string Ingredients { get => Ingredients; set => Ingredients = value; }
+        public string Instructions { get => Instructions; set => Instructions = value; }
+    }
+}
